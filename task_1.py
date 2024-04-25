8f73960ca492c9d1e0d8a3114cb4192bea6a8224
Yevhenii Baranov <jask.b@outlook.com>
2024-04-25 23:32:48 +0200

    comments for task_1 added

--- task_1.py
+++ task_1.py
@@ -1,18 +1,37 @@
 def total_salary(path: str) -> tuple:
+    """Calculates the total salary of all developers and returns the total and average wage.
+
+    Args:
+        path (str): The file path containing the developer salaries. Each line contains 
+        a comma-separated value representing the developer's name (ignored in this function)
+        and their salary.
+
+    Returns:
+        tuple: A tuple containing the total salary of all developers and the average wage.
+        If an error occurs, it returns '0' as the total salary and 'unavailable' for the 
+        average wage.
+    """
     try:
         with open(path, 'r', encoding='utf-8') as file:
-            total = 0
+            total_payment = 0
             persons = 0
-            for line in file:
-                personal_salary = int(line.split(",")[1])
-                total += personal_salary
+            for dev_payment in file:
+                personal_wage = int(dev_payment.split(",")[1])
+                total_payment += personal_wage
                 persons += 1
-            average = int(total) / persons
-            return (total, int(average)) 
+            average_wage = int(total_payment) / persons
+            return (total_payment, int(average_wage)) 
 
     except Exception as e:
         return ('0', 'unavailable')
 
 
-total, average = total_salary("salary_file.txt")
+# Test run
+total, average = total_salary("task_1/salary_file.txt")
+print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
+
+total, average = total_salary("task_1/salary_file_1.txt")
+print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
+
+total, average = total_salary("task_1/salary_file_2.txt")
 print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
